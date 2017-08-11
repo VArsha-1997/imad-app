3699dc93dfab0f294932db55c924a544aabb98f9
VArsha-1997 <30746282+VArsha-1997@users.noreply.github.com>
2017-08-11 13:01:56 -0600

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articleOne=
+var articles={
+'article-one':
 { title: 'Article One | VArsha-1997',
 heading: 'Article one',
 date: ' August 10,2017',
@@ -18,7 +19,24 @@ content:` <p>
              <p>
                 This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.This is the content of my first article.
             </p>  `
+},
+'article-two':
+{title: 'Article two | VArsha-1997',
+heading: 'Article Two',
+date: ' August 10,2017',
+content:` <p>
+                This is the content of my second article.
+            </p>  `
+},
+'article-three':
+{title: 'Article Three | VArsha-1997',
+heading: 'Article Three',
+date: ' August 10,2017',
+content:` <p>
+                This is the content of my third article.
+            </p>  `},
 };
+
 function createTemplate(data) {
     var title = data.title;
     var heading=data.heading;
@@ -58,15 +76,13 @@ return htmltemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one', function(req,res){
-     res.send(createTemplate(articleOne));
+app.get('/:articleName', function(req,res){
+    //articleName == article-one
+    articles[articleName] == {} content object for article one
+  var articleName = req.parans.articleName;  
+     res.send(createTemplate(articles[articleName]));
 });
-app.get('/article-two', function(req,res){
-    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
-app.get('/article-three', function(req,res){
-     res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
