60d6781f908e8dae9fbdf706ae7ae7fe8cd555bd
VArsha-1997 <30746282+VArsha-1997@users.noreply.github.com>
2017-08-13 04:32:52 -0600

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,7 +4,7 @@ var element=document.getElementById('main-text');
 element.innerHTML = 'New value';
 // move the image
 var img= document.getElementById('madi');
-var marginLeft = '0';
+var marginLeft = 0;
 function moveRight(){
     marginLeft=marginLeft+1;
     img.style.marginLeft= marginLeft+'px';
